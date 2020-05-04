9e42ad1be87b6507229d5c1425103a907348cda8
cg4213 <cg4213@aliyun.com>
2020-05-04 11:42:31 +0800

    batcher 测试资源

---
+++ Assets/CustomSRP/DevTestAssets/RandomSpawner.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+    public int num;
+    public List<GameObject> spawned = new List<GameObject> ();
+    [Button]
+    void Spawn ()
+    {
+        for (int i = 0; i < num; ++i)
+        {
+            var newInst = GameObject.Instantiate (prefab);
+
+            spawned.Add (newInst);
+            newInst.transform.SetParent (transform);
+            OnSpawned (newInst);
+        }
+    }
+
+    [Button]
+    void Clear ()
+    {
+
+        foreach (var item in spawned)
+        {
+            if (item == null)
+                continue;
+            if (Application.isPlaying)
+            {
+                Object.Destroy (item);
+            }
+            else
+            {
+                Object.DestroyImmediate (item);
+            }
+        }
+
+        spawned.Clear ();
+    }
+
+    public float raduis = 10;
+    public List<Material> materials;
+    void OnSpawned (GameObject newInst)
+    {
+        var position = Random.insideUnitSphere * raduis;
+        var index = Random.Range (0, materials.Count);
+
+        newInst.transform.position = position;
+        var renderer = newInst.GetComponent<Renderer> ();
+        renderer.sharedMaterial = materials[index];
+    }
+}
